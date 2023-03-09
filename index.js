bdacedd75dce674da17b8cca56acfc8ebe6796f2
Yasir Mohamed <105544374+Yasirwiifto@users.noreply.github.com>
2023-03-09 12:06:50 +0000

    add: new declaration

---
+++ index.js
@@ -0,0 +1,7 @@
+// if the costumer is under 18 they are not eligible.
+// if the costumer is above 18 they are eligible.
+
+let age = 18
+let type = age > 17 ? 'Eligible' : 'Not Eligible';
+
+console.log(age);
